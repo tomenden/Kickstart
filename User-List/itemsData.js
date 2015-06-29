d42544982f5a5eeef5b1de430149da6005bee73f
Tom Enden <tome@wix.com>
2015-06-29 22:12:12 +0300

    added User-List exercise

---
+++ User-List/itemsData.js
@@ -0,0 +1,61 @@
+/**
+ * Created by tome on 6/29/15.
+ */
+
+var ITEMS = [
+    {
+        "id": 1,
+        price: "15$",
+        name: "Item 1"
+    },
+    {
+        "id": 2,
+        price: "21$",
+        name: "Item 2"
+    },
+    {
+        "id": 3,
+        price: "32$",
+        name: "Item 3"
+    },
+    {
+        "id": 4,
+        price: "491$",
+        name: "Item 4"
+    },
+    {
+        "id": 5,
+        price: "15$",
+        name: "Item 5"
+    },
+    {
+        "id": 6,
+        price: "21$",
+        name: "Item 6"
+    },
+    {
+        "id": 7,
+        price: "32$",
+        name: "Item 7"
+    },
+    {
+        "id": 8,
+        price: "491$",
+        name: "Item 8"
+    },
+    {
+        "id": 9,
+        price: "21$",
+        name: "Item 9"
+    },
+    {
+        "id": 10,
+        price: "32$",
+        name: "Item 10"
+    },
+    {
+        "id": 11,
+        price: "491$",
+        name: "Item 11"
+    }
+];
---
+++ User-List/itemsScript.js
@@ -0,0 +1,104 @@
+/**
+ * Created by tome on 6/29/15.
+ */
+var tBody = document.querySelector('tbody');
+var fragment = document.createDocumentFragment();
+var headers = document.querySelectorAll('th');
+var headersOrder = [];
+var itemsPerPage = 5; //TODO: Move to input for user selection
+var itemCount = ITEMS.length;
+var pagesCount = Math.ceil(itemCount / itemsPerPage);
+
+function createSelectElement(arrayLength, index) {
+    var selectElement = document.createElement('select');
+    selectElement.setAttribute('onchange', 'handleChangeRow(this)');
+    fillOptions(selectElement, arrayLength, index);
+    return selectElement;
+}
+
+function fillOptions(selectElement, arrayLength, index) {
+    for (var row = 1; row <= arrayLength; row++) {
+        var optionElement = document.createElement('option');
+        optionElement.setAttribute('value', row.toString());
+        optionElement.textContent = row;
+        selectElement.appendChild(optionElement);
+    }
+    selectElement.selectedIndex = index;
+
+}
+
+(function getHeadersOrder() {
+    for (var i = 0; i < headers.length; i++) {
+        headersOrder.push(headers[i].dataset.field);
+    }
+})();
+
+
+function updateSelected() {
+    var allSelects = tBody.querySelectorAll('select');
+    for (var i = 0; i < allSelects.length; i++) {
+        allSelects[i].selectedIndex = allSelects[i].parentElement.parentElement.sectionRowIndex;
+    }
+}
+
+function handleChangeRow(select) {
+    var selected = select.value;
+    var tr = tBody.removeChild(select.parentElement.parentElement);
+    var newRow = document.querySelector('tbody > tr:nth-child(' + selected + ')');
+    if (newRow) {
+        tBody.insertBefore(tr, newRow);
+    } else {
+        tBody.appendChild(tr);
+    }
+    updateSelected();
+}
+
+function createPaginationDiv() {
+    var tFoot = document.querySelector('tFoot');
+    var paginationFragment = document.createDocumentFragment();
+    var div = document.createElement('div');
+    for (var i = 0; i < pagesCount; i++) {
+        var pageNum = i + 1;
+        var button = document.createElement('button');
+        button.setAttribute('onclick', 'goToPage(' + pageNum + ')');
+        button.textContent = pageNum;
+        div.appendChild(button);
+    }
+    paginationFragment.appendChild(div);
+    tFoot.appendChild(paginationFragment);
+};
+
+function createPageFragment(pageNumber) {
+    var pageFragment = document.createDocumentFragment();
+    var start = pageNumber * itemsPerPage - itemsPerPage;
+    var end = (start + itemsPerPage) < itemCount ? start + itemsPerPage : itemCount;
+    for (var i = start; i < end; i++) {
+        var item = ITEMS[i];
+        var tr = document.createElement('tr');
+        for (var j = 0; j < headersOrder.length; j++) {
+            var td = document.createElement('td');
+            if (headersOrder[j] === 'select-row') {
+                td.appendChild(createSelectElement(end - start, i));
+            } else {
+                td.textContent = item[headersOrder[j]];
+            }
+            tr.appendChild(td);
+        }
+
+        pageFragment.appendChild(tr);
+    }
+    return pageFragment;
+}
+
+function goToPage(pageNumber) {
+    var pageFragment = createPageFragment(pageNumber);
+    tBody.innerHTML = ""; // reset tBody
+    tBody.appendChild(pageFragment);
+    updateSelected();
+}
+
+(function init() {
+    goToPage(1);
+    createPaginationDiv();
+})();
+
