f0a0d8484d01b06397485cdff28c7041f4dd7e49
Tom Enden <tome@wix.com>
2015-07-04 11:07:57 +0300

    updated shopping cart v2

---
+++ Shopping-Cart-v2/data/inventory.js
@@ -0,0 +1,201 @@
+/**
+ * Created by tome on 7/3/15.
+ */
+var inventory = [
+    {
+        "description": "Laborum veniam aliquip mollit reprehenderit ad id. Qui mollit amet voluptate consequat eiusmod consequat aliquip mollit minim fugiat nulla laborum dolor. Incididunt laborum elit laborum ad ad reprehenderit laboris consectetur dolor officia occaecat non Lorem. ",
+        "price": "2682$",
+        "id": "55927eac85594c45b02c5963",
+        "stock": 20,
+        "image": "http://placehold.it/32x32",
+        "name": "reprehenderit"
+    }, {
+        "description": "Duis duis eiusmod cupidatat sint ullamco non ut. Adipisicing pariatur eu eu id amet mollit tempor eiusmod ipsum esse ad elit non. In amet cillum esse nostrud cillum anim consequat aute occaecat. ",
+        "price": "6667$",
+        "id": "55927eacf347ad1e77801bcb",
+        "stock": 24,
+        "image": "http://placehold.it/32x32",
+        "name": "pariatur"
+    }, {
+        "description": "Voluptate eiusmod id sit in adipisicing tempor ipsum. Mollit ut amet sint reprehenderit anim incididunt. Pariatur labore tempor eu magna anim exercitation ea adipisicing exercitation adipisicing id aliquip. Amet esse fugiat anim do deserunt veniam voluptate enim duis in labore aliquip est. Ut mollit ullamco veniam consectetur aute sunt sunt consequat culpa nulla tempor quis. Fugiat ut adipisicing sint dolore consequat in nisi id nostrud ipsum. ",
+        "price": "4339$",
+        "id": "55927eac3fe03090e7fcf966",
+        "stock": 33,
+        "image": "http://placehold.it/32x32",
+        "name": "aliquip"
+    }, {
+        "description": "Anim nulla adipisicing reprehenderit laboris sunt nostrud incididunt pariatur nostrud. Laboris consequat cupidatat sit ea exercitation duis dolor. Cillum nostrud et cupidatat laboris culpa tempor non duis eu sint dolor aliquip. Labore magna aliquip commodo magna aliqua. Dolore incididunt dolore ea dolor consectetur consectetur do consectetur cillum culpa. Adipisicing in pariatur minim occaecat. ",
+        "price": "584$",
+        "id": "55927eac01cd0f1cec513a04",
+        "stock": 21,
+        "image": "http://placehold.it/32x32",
+        "name": "esse"
+    }, {
+        "description": "Anim ut ad do aliqua minim ea nostrud dolore eu laboris cupidatat incididunt. Eu cupidatat esse ex aliquip duis aliqua. Excepteur ad elit do ex occaecat aliquip elit excepteur. Adipisicing aliquip Lorem id non et et quis dolor cillum ipsum culpa do. Adipisicing et quis Lorem in eiusmod ea in cillum. ",
+        "price": "9763$",
+        "id": "55927eace5869bb53af93629",
+        "stock": 22,
+        "image": "http://placehold.it/32x32",
+        "name": "fugiat"
+    }, {
+        "description": "Consectetur sit irure consectetur sunt adipisicing est nisi labore reprehenderit cupidatat aute ipsum. Excepteur ex tempor enim anim cupidatat ex deserunt veniam. Anim proident ut veniam occaecat amet eu ea consectetur. Sunt sunt enim eiusmod tempor anim incididunt veniam nostrud fugiat. Magna ullamco enim adipisicing pariatur incididunt eiusmod velit do. ",
+        "price": "9699$",
+        "id": "55927eac289234989c56e84f",
+        "stock": 33,
+        "image": "http://placehold.it/32x32",
+        "name": "cupidatat"
+    }, {
+        "description": "Tempor quis elit proident tempor ullamco adipisicing ut duis magna sunt adipisicing dolor do mollit. Exercitation eiusmod cillum id irure do. Minim pariatur commodo pariatur commodo fugiat sint est pariatur consequat id. ",
+        "price": "5220$",
+        "id": "55927eac18475131d0342144",
+        "stock": 27,
+        "image": "http://placehold.it/32x32",
+        "name": "voluptate"
+    }, {
+        "description": "Voluptate voluptate esse non est sint reprehenderit voluptate occaecat eiusmod consequat. Velit cillum esse labore consectetur veniam sit tempor quis duis dolor laboris laboris. Qui ad culpa enim proident consequat aute anim dolor mollit laboris commodo dolor cillum ullamco. Magna ut amet non amet eiusmod nulla laboris velit voluptate. Non do dolor id aute est cupidatat tempor et. Cupidatat enim sit enim labore ipsum culpa pariatur aliqua ex ut veniam consectetur. ",
+        "price": "6548$",
+        "id": "55927eacab39a216521c1e07",
+        "stock": 33,
+        "image": "http://placehold.it/32x32",
+        "name": "consequat"
+    }, {
+        "description": "Minim quis eu laboris ipsum eiusmod velit qui exercitation ad nisi consequat nulla duis nisi. Sit adipisicing cupidatat laboris aliqua velit est sint mollit sit consequat occaecat. Velit voluptate proident adipisicing aute dolore velit culpa. Sit duis esse esse est ad eu ex exercitation culpa et ullamco reprehenderit consequat aliqua. Duis mollit veniam magna non minim tempor proident in officia incididunt. ",
+        "price": "2538$",
+        "id": "55927eacd53d3a6cf43d1da0",
+        "stock": 36,
+        "image": "http://placehold.it/32x32",
+        "name": "ea"
+    }, {
+        "description": "Nostrud adipisicing mollit minim consequat qui ad consectetur nostrud. Et commodo consectetur commodo culpa amet proident ipsum quis dolor. Occaecat eiusmod irure ea do aute tempor. Adipisicing velit Lorem veniam qui laboris. ",
+        "price": "8702$",
+        "id": "55927eacdd2b5dcb9374a928",
+        "stock": 35,
+        "image": "http://placehold.it/32x32",
+        "name": "et"
+    }, {
+        "description": "Ullamco et officia sit elit occaecat duis magna fugiat deserunt ipsum do enim sunt enim. Fugiat eu ad nostrud cillum eiusmod do nisi ea anim nostrud occaecat aliqua aliquip aute. Incididunt magna minim non excepteur esse deserunt quis id. Id est amet eiusmod fugiat officia culpa. ",
+        "price": "4765$",
+        "id": "55927eac78b393c4253e91f0",
+        "stock": 29,
+        "image": "http://placehold.it/32x32",
+        "name": "fugiat"
+    }, {
+        "description": "Veniam ipsum magna sunt sint reprehenderit. Veniam exercitation mollit non cillum anim id et laboris reprehenderit sunt ad sit amet ipsum. Sunt Lorem magna eu laborum deserunt ex elit et. Reprehenderit pariatur adipisicing amet commodo Lorem esse exercitation magna tempor ut est voluptate cupidatat aliquip. Aute nisi proident ullamco deserunt consectetur id amet. ",
+        "price": "7629$",
+        "id": "55927eac43f29ceed849fc79",
+        "stock": 21,
+        "image": "http://placehold.it/32x32",
+        "name": "labore"
+    }, {
+        "description": "Reprehenderit duis id cillum proident consectetur deserunt veniam minim id dolor sunt amet. Lorem aliqua exercitation duis consequat ut aliqua esse. Aute veniam id excepteur deserunt in occaecat non anim fugiat labore. ",
+        "price": "5039$",
+        "id": "55927eacf76261770dcc06d5",
+        "stock": 25,
+        "image": "http://placehold.it/32x32",
+        "name": "adipisicing"
+    }, {
+        "description": "Aliquip ullamco sunt qui mollit. Fugiat ad voluptate est aliqua magna eiusmod proident excepteur consequat esse. Tempor irure incididunt eiusmod veniam voluptate laboris amet. Fugiat ullamco ex sit adipisicing exercitation et. Reprehenderit amet occaecat anim aute. ",
+        "price": "5102$",
+        "id": "55927eac6da99d0c61e5d31a",
+        "stock": 34,
+        "image": "http://placehold.it/32x32",
+        "name": "occaecat"
+    }, {
+        "description": "Culpa irure quis minim labore exercitation. Deserunt nostrud occaecat commodo labore occaecat culpa ea aliqua sint quis velit. Ea aliquip aliquip consectetur deserunt deserunt. Ut enim reprehenderit ipsum non elit dolore id laboris exercitation dolor ex aliqua. Anim aliquip mollit sint Lorem do excepteur officia aliquip sit ut nostrud pariatur ipsum ex. Occaecat duis commodo occaecat consequat culpa deserunt officia laborum enim. Dolore laborum fugiat dolor dolor exercitation commodo id et sunt adipisicing laboris occaecat proident. ",
+        "price": "1511$",
+        "id": "55927eaca917aed17f9efc8d",
+        "stock": 21,
+        "image": "http://placehold.it/32x32",
+        "name": "excepteur"
+    }, {
+        "description": "Dolor aute laborum non mollit exercitation voluptate excepteur Lorem voluptate minim occaecat. Esse dolore qui ullamco elit ex nulla sit. Reprehenderit aute ea deserunt ex aliqua velit quis. Irure voluptate incididunt minim minim nostrud duis commodo. Est velit esse tempor et exercitation nulla minim irure nostrud id occaecat. ",
+        "price": "8904$",
+        "id": "55927eac644bf2c6e10d1c3f",
+        "stock": 40,
+        "image": "http://placehold.it/32x32",
+        "name": "consectetur"
+    }, {
+        "description": "Laboris id enim ea culpa anim deserunt duis velit est magna amet reprehenderit. Amet occaecat ex voluptate eiusmod consequat nostrud non fugiat elit consequat. Ad cupidatat amet mollit nostrud sunt. ",
+        "price": "5737$",
+        "id": "55927eac23185fdc5f50bf66",
+        "stock": 33,
+        "image": "http://placehold.it/32x32",
+        "name": "esse"
+    }, {
+        "description": "Commodo ad non velit do consectetur aliquip fugiat excepteur aliqua officia minim. Occaecat consectetur do ut occaecat irure et in occaecat sunt eu qui enim consequat. Dolor ut officia consequat fugiat adipisicing aliquip incididunt quis ea duis. Ut deserunt et reprehenderit eu adipisicing. In laboris sit aliquip consequat tempor adipisicing commodo dolor est ex anim sint. ",
+        "price": "9265$",
+        "id": "55927eac1f5e780e9b949a61",
+        "stock": 25,
+        "image": "http://placehold.it/32x32",
+        "name": "esse"
+    }, {
+        "description": "Eiusmod deserunt mollit est tempor laborum deserunt magna nostrud veniam aliquip magna deserunt dolor velit. Cupidatat culpa ut commodo minim incididunt. Duis occaecat eiusmod id minim duis exercitation laboris amet in aliquip eu deserunt do cupidatat. ",
+        "price": "7800$",
+        "id": "55927eac072e17c0b43a2234",
+        "stock": 29,
+        "image": "http://placehold.it/32x32",
+        "name": "enim"
+    }, {
+        "description": "Sit qui velit deserunt exercitation dolore velit dolore labore veniam non commodo eiusmod commodo. Officia do laborum amet irure qui reprehenderit. Voluptate aliquip aute voluptate cillum tempor ex reprehenderit veniam. ",
+        "price": "6916$",
+        "id": "55927eac5774888947ebffbf",
+        "stock": 21,
+        "image": "http://placehold.it/32x32",
+        "name": "cillum"
+    }, {
+        "description": "Voluptate enim eu do magna qui occaecat cupidatat cillum. Incididunt do amet velit sunt fugiat. Cupidatat aute tempor consectetur cillum duis. Elit reprehenderit ullamco ad exercitation reprehenderit proident veniam adipisicing enim sunt occaecat culpa aute. ",
+        "price": "2479$",
+        "id": "55927eac437343d3acf3cf97",
+        "stock": 31,
+        "image": "http://placehold.it/32x32",
+        "name": "in"
+    }, {
+        "description": "Officia exercitation pariatur Lorem occaecat cupidatat ex incididunt ut minim dolor sunt. Ipsum cillum deserunt sint sunt occaecat fugiat. Voluptate ex qui proident proident. Culpa officia deserunt enim dolore minim aliqua. Id id ullamco voluptate reprehenderit mollit deserunt proident magna. Veniam duis id aliqua tempor in exercitation laborum Lorem magna. Velit ex anim cupidatat consequat mollit laboris et. ",
+        "price": "7045$",
+        "id": "55927eaccf98e2897296bc38",
+        "stock": 29,
+        "image": "http://placehold.it/32x32",
+        "name": "officia"
+    }, {
+        "description": "Minim officia non laboris tempor anim laboris excepteur velit occaecat ad incididunt. Lorem esse sunt pariatur consequat dolor minim ullamco sit. Occaecat cillum et aliqua ullamco ea anim velit esse duis veniam. Cillum duis eiusmod occaecat do fugiat ut. Occaecat deserunt cupidatat adipisicing reprehenderit. In aliquip Lorem aliquip irure. ",
+        "price": "8033$",
+        "id": "55927eace4f7eca209e075f5",
+        "stock": 24,
+        "image": "http://placehold.it/32x32",
+        "name": "magna"
+    }, {
+        "description": "Adipisicing nostrud pariatur fugiat dolore voluptate ad non ex. Reprehenderit reprehenderit ad velit irure laboris sit. Voluptate cillum pariatur dolor labore qui id non sint occaecat anim occaecat aliqua. Occaecat veniam veniam aliqua Lorem nulla veniam ex cillum sunt occaecat exercitation pariatur veniam adipisicing. Irure incididunt enim deserunt pariatur id occaecat in elit irure exercitation. ",
+        "price": "1088$",
+        "id": "55927eac53c57224803d56c0",
+        "stock": 35,
+        "image": "http://placehold.it/32x32",
+        "name": "quis"
+    }, {
+        "description": "Est ex pariatur eu veniam ea in. Labore duis officia ut enim. Cillum adipisicing ut deserunt dolor minim enim mollit velit labore laboris sit. Dolor anim duis cillum ut commodo dolore amet dolore dolore ea cillum aute nisi. Nostrud sunt enim consectetur irure sit aute consectetur ipsum in aute laboris ad mollit fugiat. ",
+        "price": "9778$",
+        "id": "55927eac1cf8fa3fe5cdb419",
+        "stock": 34,
+        "image": "http://placehold.it/32x32",
+        "name": "consequat"
+    }, {
+        "description": "Voluptate esse veniam magna ea ex. Qui eiusmod proident nisi nulla ad enim aute do laboris quis ad velit qui. Pariatur id ipsum sint consequat proident culpa culpa commodo. Ut amet cillum laboris anim sit et pariatur proident. Labore in Lorem eu exercitation eu adipisicing aliqua sint voluptate esse. Enim non cillum reprehenderit consectetur. Ut dolore commodo tempor commodo deserunt. ",
+        "price": "5196$",
+        "id": "55927eacecfd7399a9e335c0",
+        "stock": 21,
+        "image": "http://placehold.it/32x32",
+        "name": "eiusmod"
+    }, {
+        "description": "Cillum dolor ex veniam sit elit aliqua minim eu sint. Nisi velit cillum tempor in velit in. Laborum aliqua magna labore velit tempor laboris ullamco aute esse sint. Exercitation et esse pariatur do ex. Culpa adipisicing ipsum enim mollit aliqua id labore nostrud. Et quis magna enim ad id ut non qui nisi reprehenderit officia occaecat labore. ",
+        "price": "8987$",
+        "id": "55927eac078064168962fdfe",
+        "stock": 28,
+        "image": "http://placehold.it/32x32",
+        "name": "nostrud"
+    }, {
+        "description": "Voluptate minim enim proident eu pariatur in quis dolore veniam amet nostrud dolore. Cupidatat et consectetur labore minim mollit. Dolore laborum amet quis id ipsum quis enim amet mollit quis aliquip nostrud qui. Tempor quis in do ipsum duis est non. Deserunt magna esse anim nulla commodo ex. Duis quis nulla esse labore ipsum nulla qui velit. ",
+        "price": "1877$",
+        "id": "55927eaccff653b8e5efc711",
+        "stock": 20,
+        "image": "http://placehold.it/32x32",
+        "name": "do"
+    }];
--- Shopping-Cart-v2/js/app.js
+++ Shopping-Cart-v2/js/app.js
@@ -1,3 +1,243 @@
 /**
  * Created by tome on 7/3/15.
  */
+
+// Data variable - inventory (located at data/)
+
+/********Helpers****************************************************************************************************************************/
+function getItemFromId(id) {
+    for (var i = 0; i < inventory.length; i++) {
+        var item = inventory[i];
+        if (item.id === id) {
+            console.log('success, item id: ' + id);
+            return item;
+        }
+    }
+    console.log('could not find item with id: ' + id);
+    return false;
+}
+function getItemWithQuantity(itemId, quantity) {
+    var item = getItemFromId(itemId);
+    if (quantity > item.stock) {
+        console.log('No more soup for you!');
+    }
+    else {
+        item.quantity = quantity;
+    }
+    return item;
+}
+function itemAlreadySelected(itemId) {
+    var selectedItems = shoppingSession.selectedItems;
+    for (var i = 0; i < selectedItems.length; i++) {
+        if (selectedItems[i].id === itemId) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/********Pub/Sub****************************************************************************************************************************/
+
+var counter = 0;
+var subscriptions = {};
+function subscribe(eventType, callback) {
+    subscriptions[eventType] = subscriptions[eventType] || [];
+    subscriptions[eventType].push(callback);
+
+}
+function publish(eventType, args) {
+    var subscribers = subscriptions[eventType];
+    for (var i = 0; i < subscribers.length; i++) {
+        subscribers[i](args);
+    }
+}
+function unsubscribe(eventType, uniqueID) {
+    if (subscriptions[eventType] && subscriptions[eventType][uniqueID]) {
+        delete subscriptions[eventType][uniqueID];
+    }
+}
+
+
+// shopping session state: {selectedItems:[item (withQuantity, id), item], inCart: [{item, quantity}]
+/********Shopping Session****************************************************************************************************************************/
+
+var shoppingSession = {
+    selectedItems: [],
+    inCart: []
+};
+
+/********Increment & Decrement****************************************************************************************************************************/
+
+function incrementItem(itemId) {
+    if (itemAlreadySelected(itemId)) {
+        getItemFromId(itemId).quantity += 1;
+    }
+    else {
+        shoppingSession.selectedItems.push(getItemWithQuantity(itemId, 1));
+    }
+    publish('quantityChanged', itemId);
+    return shoppingSession.selectedItems;
+}
+
+function decrementItem(itemId) {
+    if (itemAlreadySelected(itemId)) {
+        var item = getItemFromId(itemId);
+        if (item.quantity - 1 === 0) {
+            var i = shoppingSession.selectedItems.indexOf(item);
+            shoppingSession.selectedItems.splice(i, 1);
+            delete item.quantity;
+            console.log('Item: ' + itemId + ' quantity has been removed')
+        }
+        else {
+            item.quantity--;
+            console.log('new quantity: ' + item.quantity);
+        }
+        publish('quantityChanged', itemId);
+    }
+    else {
+        console.log('item is not selected, and thus can not be decremented');
+    }
+    return shoppingSession.selectedItems;
+}
+
+// Subscribe increment and decrement events to
+
+
+
+// Shop Window Constructor. There will be one instance of shop window.
+// To change the items displayed in the window use method displayNewItems([ids])
+function ShopWindow(ids) {//array of item ids
+    this.ids = ids;
+    this.headers = ['name', 'description', 'image', 'price', 'quantity'];
+    this.displayData = [];
+    this.update = function () {
+        var itemsDisplayData = [];
+        for (var i = 0; i < this.ids.length; i++) {
+            var obj = {};
+            var item = getItemFromId(this.ids[i]);
+            for (var j = 0; j < this.headers.length; j++) {
+                var header = this.headers[j];
+                if (item[header]) {
+                    obj[header] = item[header];
+                }
+            }
+            itemsDisplayData.push(obj);
+        }
+        this.displayData = itemsDisplayData;
+        console.log(itemsDisplayData);
+        return itemsDisplayData;
+    }.bind(this);
+    this.subscribe = function() {
+        subscribe('quantityChanged', this.update);
+        subscribe('shopWindowChanged', this.update);
+        subscribe('updatedItemsOnPage', this.displayNewItems);//Subscribe to pagination change/update
+    };
+    this.displayNewItems = function (ids) {
+        this.ids = ids;
+        publish('shopWindowChanged');
+        return 'success';
+    }.bind(this);
+
+    this.addToCart = function (itemId) {
+        var item = getItemFromId(itemId);
+        if (item.quantity) {
+            publish('addToCartButtonPressed', item);
+        }
+    };
+    //Initialize
+    this.subscribe();
+}
+
+ShopWindow = new ShopWindow();
+
+
+
+
+// pagination
+function Pagination(currentPage, itemsPerPage) {
+    this.currentPage = currentPage || 1;
+    this.itemsPerPage = itemsPerPage || 5;
+    this.getCurrentPageItemsIds = function () {
+        var ids = [];
+        for (var i = this.firstItemIndex; i < this.lastItemIndex; i++) {
+            ids.push(inventory[i].id);
+        }
+        return ids;
+    }.bind(this);
+    this.update = function () {
+        this.numberOfPages = Math.ceil(inventory.length / this.itemsPerPage);
+        this.firstItemIndex = this.currentPage * this.itemsPerPage - this.itemsPerPage;
+        this.lastItemIndex = this.firstItemIndex + this.itemsPerPage;
+        if (this.lastItemIndex > inventory.length) {
+            this.lastItemIndex = inventory.length;
+        }
+        publish('updatedItemsOnPage', this.getCurrentPageItemsIds());
+    }.bind(this);
+    this.goToPage = function (pageNumber) {
+        this.currentPage = pageNumber;
+        publish('changePage');
+    }.bind(this);
+    this.changeItemsPerPage = function(limit) {
+        this.itemsPerPage = limit;
+        publish('changeItemsPerPage');
+    }.bind(this);
+    this.subscribe = function () {
+        subscribe('changePage', this.update);
+        subscribe('changeItemsPerPage', this.update);
+    };
+
+    //init
+    this.update();
+    this.subscribe();
+}
+
+Pagination = new Pagination(1, 5);
+
+//Helper
+function itemIndexInCart(itemId, cart) {
+    for (var i = 0; i < cart.itemsInCart.length; i++) {
+        if (cart.itemsInCart[i].id === itemId) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function ShoppingCart() {
+    this.itemsInCart = [];
+    this.addItemToCart = function (item) {
+        var indexInCart = itemIndexInCart(item.id, this);
+        if (indexInCart > -1) {
+            delete this.itemsInCart[indexInCart];
+        }
+        var itemObj = {
+            id: item.id,
+            name: item.name,
+            quantity: item.quantity,
+            price: parseInt(item.quantity, 10) * parseInt(item.price, 10)
+        };
+        this.itemsInCart.push(itemObj);
+        publish('Added item to cart');
+    }.bind(this);
+
+    this.getTotal = function () {
+        var quantity = 0, price = 0;
+        for (var i = 0; i < this.itemsInCart.length; i++) {
+            quantity += this.itemsInCart[i].quantity;
+            price += parseInt(this.itemsInCart[i].price, 10);
+        }
+        this.totalQuantity = quantity;
+        this.totalPrice = price;
+        return {'Total Quantity': quantity, 'Total Price': price};
+    }.bind(this);
+
+
+    this.subscribe = function () {
+        subscribe('addToCartButtonPressed', this.addItemToCart);
+        subscribe('Added item to cart', this.getTotal)
+    };
+
+    this.subscribe();
+}
+
+ShoppingCart = new ShoppingCart();
